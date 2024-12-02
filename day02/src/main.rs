bd1fa33cda3e728bb86eac64fe819bec38a7fb88
UnikMask <alexandre.guerville@hotmail.com>
2024-12-02 21:30:31 +0000

    refactor: Cleanup day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,31 +1,26 @@
 use advent_of_code_2024_utils::load_file;
 
 fn main() {
-    let safes = load_file("day02/input.txt")
+    let sequences = load_file("day02/input.txt")
         .iter()
         .map(|e| {
             e.split_whitespace()
                 .map(|s| s.parse::<i64>().unwrap())
                 .collect::<Vec<_>>()
         })
+        .collect::<Vec<_>>();
+    let sln1 = sequences
+        .iter()
         .filter(|v| get_errors_rec(None, None, None, v) == 0)
-        .collect::<Vec<_>>();
-    println!("sln 1: {}", safes.len());
-
-    let safes_hold_one = load_file("day02/input.txt")
+        .collect::<Vec<_>>()
+        .len();
+    println!("sln 1: {}", sln1);
+    let sln2 = sequences
         .iter()
-        .map(|e| {
-            e.split_whitespace()
-                .map(|s| s.parse::<i64>().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .filter(|v| {
-            get_errors_rec(None, None, None, v).min(get_errors_rec(None, None, None, &v[1..]) + 1)
-                <= 1
-        })
-        .collect::<Vec<_>>();
-
-    println!("sln 2: {}", safes_hold_one.len());
+        .filter(|v| get_errors_rec(None, None, None, v) <= 1)
+        .collect::<Vec<_>>()
+        .len();
+    println!("sln 2: {}", sln2);
 }
 
 fn is_correct(i: i64, last: Option<i64>, plast: Option<i64>) -> bool {
@@ -49,16 +44,19 @@ fn get_errors_rec(
     numbers: &[i64],
 ) -> u32 {
     let i = numbers[0];
-    if numbers.len() == 1 {
-        return (!is_correct(i, last, plast)).into();
-    }
-
-    if !is_correct(i, last, plast) {
-        let mut res = get_errors_rec(last, plast, pplast, &numbers[1..]);
-        if is_correct(i, plast, pplast) {
-            res = res.min(get_errors_rec(Some(i), plast, pplast, &numbers[1..]));
+    let errored = !is_correct(i, last, plast);
+    match (numbers.len(), errored) {
+        (1, _) => errored.into(),
+        (_, true) => {
+            let mut res = get_errors_rec(last, plast, pplast, &numbers[1..]);
+            if is_correct(i, plast, pplast) {
+                res = res.min(get_errors_rec(Some(i), plast, pplast, &numbers[1..]));
+            }
+            if is_correct(i, last, pplast) {
+                res = res.min(get_errors_rec(Some(i), last, pplast, &numbers[1..]));
+            }
+            res + 1
         }
-        return res + 1;
+        (_, false) => get_errors_rec(Some(i), last, plast, &numbers[1..]),
     }
-    get_errors_rec(Some(i), last, plast, &numbers[1..])
 }
